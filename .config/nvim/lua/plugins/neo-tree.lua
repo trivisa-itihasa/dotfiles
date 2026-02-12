ebc253837e4afd5f161787effb690bd577dfb1c1
trivisa.itihasa <trivisa.itihasa@gmail.com>
2026-02-12 18:15:08 +0900

    [Edit] Change order of roading plugins

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     filesystem = {
       filtered_items = {
-        visible = false,
+        visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
         hide_gitignored = false,
