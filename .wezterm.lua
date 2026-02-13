8e32fceb8699e907ccfa0180bea2799c073ea563
trivisa.itihasa <trivisa.itihasa@gmail.com>
2026-02-13 13:57:41 +0900

    [Edit] changed starship and wezterm color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -100,45 +100,7 @@ config.keys = {
 }
 
 -- カラー設定
-config.colors = {
-	tab_bar = {
-		background = "#1b1f2f",
-		active_tab = {
-			bg_color = "#444b71",
-			fg_color = "#c6c8d1",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab = {
-			bg_color = "#282d3e",
-			fg_color = "#c6c8d1",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab_hover = {
-			bg_color = "#1b1f2f",
-			fg_color = "#c6c8d1",
-			intensity = "Normal",
-			underline = "None",
-			italic = true,
-			strikethrough = false,
-		},
-		new_tab = {
-			bg_color = "#1b1f2f",
-			fg_color = "#c6c8d1",
-			italic = false,
-		},
-		new_tab_hover = {
-			bg_color = "#444b71",
-			fg_color = "#c6c8d1",
-			italic = false,
-		},
-	},
-}
+config.color_scheme = "Monokai Pro (Gogh)"
 
 -- ============================================================
 -- イベントハンドラ (returnの外に出す必要があります)
